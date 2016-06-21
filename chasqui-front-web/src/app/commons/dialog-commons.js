4aef3c1a72ec897bff2d78b5826328cfaa189fa9
leo <palazzo.leonardo@gmail.com>
2016-06-21 02:20:02 -0300

    dialog common

---
+++ chasqui-front-web/src/app/commons/dialog-commons.js
@@ -0,0 +1,19 @@
+
+(function() {
+  'use strict';
+  
+  angular.module('chasqui').service('StateCommons', StateCommons);
+  function StateCommons($localStorage) {
+    var vm = this;
+    vm.ls = $localStorage;
+
+    vm.ls.token;
+    
+    vm.ls.usuario = {};
+    
+    vm.ls.pedidoSeleccionado = null;
+    
+    
+
+  }// function
+})();// anonimo
