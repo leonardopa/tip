ae2c5c635891394e0660c16530502f168fe06036
leo <palazzo.leonardo@gmail.com>
2016-07-24 00:11:58 -0300

    refactor menu

---
+++ chasqui-front-web/src/app/comercio/principal/menu.controller.js
@@ -0,0 +1,49 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('chasqui')
+    .controller('MenuController',MenuController);
+
+  /** @ngInject */
+  function MenuController( $scope,$log,$state) {
+	  $log.debug("MenuController ..... ");
+	  
+	  var vm = this;
+
+	  vm.hello = "MenuController ...  hello";
+	 
+	  vm.categorias=[];
+		 
+	  vm.ir = function (page){
+		  $log.debug("ir a ..... ",page);
+		  
+		  switch (page) { 
+          case 'como-comprar':
+              $state.go('como-comprar')
+              break;
+          case 'catalogo':
+              $state.go('catalogo')
+              break;
+          case 'emprendedores':
+              $state.go('emprendedores')
+              break;
+          case 'lista-pedidos':
+              $state.go('lista-pedidos')
+              break;
+          case 'lista-grupos':             
+              $state.go('lista-grupos')
+              break;
+          case 'login':              
+              $state.go('login')
+              break;
+          default:
+
+          }
+		  
+		  
+	  }
+	  
+ 
+  }
+})();
