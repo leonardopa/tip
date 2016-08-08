e427f5b3833286517ea901d902a5e46f165df629
leo <palazzo.leonardo@gmail.com>
2016-08-08 00:08:42 -0300

    vista de productor/res

--- chasqui-front-web/src/app/comercio/varios/emprendedores.controller.js
+++ chasqui-front-web/src/app/comercio/varios/emprendedores.controller.js
@@ -6,7 +6,7 @@
     .controller('EmprenController',EmprenController);
 
   /** @ngInject */
-  function EmprenController( $log,$stateParams,restProxy, CTE_REST) {
+  function EmprenController( $log,$stateParams,restProxy, CTE_REST,$state) {
 	  $log.debug('EmprenController ..... ',$stateParams.id); 
 	   var vm = this
 	   
@@ -15,10 +15,22 @@
 	   vm.isCollapsed = true;
 	   
 	   vm.emprendedor;
+	   vm.emprendedores;
+	   vm.isVistaUnica=false;
+	   
+	   ///////////////// Eventos
+	   
+	   vm.verMas=function (item){
+		   $log.debug("---ver mas ---",item);
+		   
+		   $state.go('emprendedores', {
+		        id: item.idVendedor
+		      });
+	   }
 	   
 	   /////////////////
 	   
-	   vm.callEmprendedor = function callEmprendedor() {
+	    function callEmprendedor() {
 			$log.debug("---callEmprendedor ---");
 
 			function doOk(response) {
@@ -31,6 +43,21 @@
 			// PASARLE el id del producto vm.idEmprendedor
 			restProxy.get(CTE_REST.productores(1),{},doOk);		    
 	   }
+	   
+	   function callEmprendedores() {
+			$log.debug("---callEmprendedor ---");
+
+			function doOk(response) {
+				$log.debug("---callEmprendedor ---",response.data);
+				//vm.categorias = response.data;
+				
+				vm.emprendedores= response.data;
+			}
+			// TODO: hacer el ID de VENDEDOR dinamico
+			// PASARLE el id del producto vm.idEmprendedor
+			restProxy.get(CTE_REST.productores(1),{},doOk);		    
+	   }
+	   
 	   
 	   
 	   ///// TODO: en realidad deberia venir dentro del productor
@@ -50,10 +77,12 @@
 	   
 	   
 	   if(vm.idEmprendedor){		   
-		   vm.callEmprendedor();
+		   callEmprendedor();
 		   callMedallas();
+		   vm.isVistaUnica=true;
 	   }else{
-		   $log.error("no llego el parametro id emprendedor");
+		   //$log.error("no llego el parametro id emprendedor");
+		   callEmprendedores();
 	   }
 	   
 	   
