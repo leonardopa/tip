9a897f6a350607d4a84c5aa3f05cdc3ff7a32e21
leo <palazzo.leonardo@gmail.com>
2016-12-03 01:25:02 -0300

    progres bar . Fix notificaciones estaba oculto

--- chasqui-front-web/src/app/index.module.js
+++ chasqui-front-web/src/app/index.module.js
@@ -4,6 +4,6 @@
 	angular.module('chasqui', [ 'ngAnimate', 'ngCookies', 'ngTouch',
 			'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ui.router',
 			'ui.bootstrap', 'toastr','ngMaterial','ngStorage','slick','ngMdIcons'
-			,'pascalprecht.translate','leaflet-directive']);
+			,'pascalprecht.translate','leaflet-directive','angular-loading-bar']);
 
 })();
